d955de89491bea7cf7959b08aa774bcc1f21692f
LuciC4s <lperezc24@miumg.edu.gt>
2025-09-25 22:06:19 -0600

    fix: se agregan archivos que se habían eliminado por error

--- database/migrations/2025_09_14_220252_create_usuarios_table.php
+++ database/migrations/2025_09_14_220252_create_usuarios_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        if (!Schema::hasTable("usuarios")) {
-            Schema::create('usuarios', function (Blueprint $table) {
-                $table->id(); // equivale a BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY
+        if(!Schema::hasTable("usuarios")){
+            Schema::create('usuarios', function(Blueprint $table){
+                $table->id();
                 $table->string('nombre', 150);
                 $table->string('email', 150)->unique();
                 $table->string('password', 255);
                 $table->enum('rol', ['admin', 'usuario'])->default('usuario');
-                $table->timestamps(); // crea columnas created_at y updated_at
+                $table->timestamps();
             });
         }
     }
-
 
     /**
      * Reverse the migrations.
