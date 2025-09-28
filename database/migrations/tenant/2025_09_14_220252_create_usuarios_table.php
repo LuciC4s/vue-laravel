8be21bb0e76be5acf04aa709f59aab1fa537f85b
LuciC4s <lperezc24@miumg.edu.gt>
2025-09-28 16:29:55 -0600

    feat: se crean todas las migraciones para el tenant

---
+++ database/migrations/tenant/2025_09_14_220252_create_usuarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if(!Schema::hasTable("usuarios")){
+            Schema::create('usuarios', function(Blueprint $table){
+                $table->id();
+                $table->string('nombre', 150);
+                $table->string('email', 150)->unique();
+                $table->string('password', 255);
+                $table->enum('rol', ['admin', 'usuario'])->default('usuario');
+                $table->string('tenant_id')->nullable(); // ID del tenant al que pertenece
+                $table->timestamps();
+                
+                // Índice para mejorar consultas por tenant
+                $table->index('tenant_id');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
